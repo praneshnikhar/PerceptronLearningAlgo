283d38a0876fafa5326726a9213c1a63a7aa27dd
praneshnikhar <praneshnikhar@gmail.com>
2025-01-01 12:37:31 +0530

    initial commit

---
+++ python projects/datascienceFromScratch/.venv/main.py
@@ -0,0 +1,43 @@
+from __future__ import division
+users = [
+{ "id": 0, "name": "Hero" },
+{ "id": 1, "name": "Dunn" },
+{ "id": 2, "name": "Sue" },
+{ "id": 3, "name": "Chi" },
+{ "id": 4, "name": "Thor" },
+{ "id": 5, "name": "Clive" },
+{ "id": 6, "name": "Hicks" },
+{ "id": 7, "name": "Devin" },
+{ "id": 8, "name": "Kate" },
+{ "id": 9, "name": "Klein" }
+]
+
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+(4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+for user in users:
+    user["friends"] = []
+
+for i, j in friendships:
+    users[i]["friends"].append(users[j])
+    users[j]["friends"].append(users[i])
+
+def number_of_friends(user):
+    """how many friends does_user_have?"""
+    return len(user["friends"])
+
+total_connections = sum(number_of_friends(user)
+                        for user in users)
+
+
+num_users = len(users)
+avg_connections  = total_connections/num_users
+
+def friends_of_friend_ids_bad(user):
+    return [foaf["id"]
+            for friend in user["friends"]
+            for foaf in friend["friends"]]
+user[0]
+
+
+
