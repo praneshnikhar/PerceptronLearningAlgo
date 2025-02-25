94a3d171aa74cf56dbc8d65405df6c6431a8eb3a
praneshnikhar <praneshnikhar@gmail.com>
2025-02-25 23:18:29 +0530

    commit

---
+++ python/codes/main.py
@@ -0,0 +1,17 @@
+num = 6
+a  = 19
+b = 7
+age = 25
+temperature = 30
+
+# print("Positive" if num > 0 else "negative"
+# result  = "Even" if num%2 ==0 else "Odd"
+# max_num = a if a > b else b
+# min_num = a if a< b else b
+# print(min_num)
+# status = "Adult" if age>= 18 else "child"
+# print(min_num)
+# print(status)
+
+weather = "Hot" if temperature >20 else "cold"
+print(weather)
